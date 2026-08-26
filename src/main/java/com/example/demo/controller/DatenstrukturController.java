e625a8c8cf1dbe992225831f229088a466491e7b
Sebastian Frank <sebastian.frk@gmx.de>
2026-08-26 13:58:00 +0200

    implemented stacklogic controller

--- src/main/java/com/example/demo/controller/DatenstrukturController.java
+++ src/main/java/com/example/demo/controller/DatenstrukturController.java
@@ -6,6 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DatenstrukturController {
 
+    private StackLogik meinStack = new StackLogik();
+
+    // (Später kommen hier noch die Instanzen für die anderen hin, z.B.:)
+    // private QueueLogik meineQueue = new QueueLogik();
+
+
+    // 2. Das GetMapping ist nur dafür da, die Seite im Browser anzuzeigen
+    @GetMapping("/stack")
+    public String zeigeStack(Model model) {
+        // Hier greift die Methode auf deine Instanz zu und gibt die Daten an HTML weiter
+        model.addAttribute("stackInhalt", meinStack.getElemente());
+        return "stack";
+    }
+
     @GetMapping("/stack")
     public String zeigeStack() { return "stack"; }
 
