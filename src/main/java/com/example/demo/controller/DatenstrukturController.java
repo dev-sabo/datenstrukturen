cda9ccff075aca3e79d52864dd7b33f5c3c22866
Sebastian Frank <sebastian.frk@gmx.de>
2026-08-26 12:31:03 +0200

    Stack HTML Page

---
+++ src/main/java/com/example/demo/controller/DatenstrukturController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DatenstrukturController {
+
+    @GetMapping("/stack")
+    public String zeigeStack() { return "stack"; }
+
+    @GetMapping("/queue")
+    public String zeigeQueue() { return "queue"; }
+
+    @GetMapping("/linked-list")
+    public String zeigeLinkedList() { return "linked-list"; }
+
+    @GetMapping("/binary-tree")
+    public String zeigeBinaryTree() { return "binary-tree"; }
+
+    @GetMapping("/hash-table")
+    public String zeigeHashTable() { return "hash-table"; }
+
+    @GetMapping("/graph")
+    public String zeigeGraph() { return "graph"; }
+}
